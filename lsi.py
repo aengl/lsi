2796a96b90dc4b332da9d82792ddc9f3752184f6
Andreas Smeria <ae@cephea.de>
2017-07-12 17:20:20 +0200

    Refactoring and experimental dialog

--- lsi.py
+++ lsi.py
@@ -34,7 +34,49 @@ def hex_to_rgb(s):
     return tuple(round(int(s.lstrip('#')[i:i + 2], 16) * mul) for i in (0, 2, 4))
 
 
+class Dialog:
+    """ A popup dialog that lets us interact with todo items.
+    """
+
+    def __init__(self):
+        self.dialog = None
+        self.close = False
+
+    def run(self):
+        """ Shows the dialog and enters a rendering loop.
+        """
+        self._init()
+        while not self.close:
+            self._render()
+            self._handle_input()
+
+    def _init(self):
+        self.dialog = curses.newwin(50, 10)
+
+    def _handle_input(self):
+        c = self.dialog.getch()
+        if c == curses.KEY_UP:
+            self._navigate(-1)
+        elif c == curses.KEY_DOWN:
+            self._navigate(1)
+        elif c == ord('q') or c == 27:  # ESC_KEY
+            self.close = True
+        elif c == ord('d') or c == curses.KEY_ENTER:
+            Dialog().run()
+
+    def _navigate(self, delta):
+        pass
+
+    def _render(self):
+        self.dialog.erase()
+        self.dialog.addstr(0, 0, 'test')
+        self.dialog.refresh()
+
+
 class TodoListViewer:
+    """ A viewer that lets us browse and filter todo items.
+    """
+
     @property
     def num_available_lines(self):
         return curses.LINES - 2
@@ -44,77 +86,78 @@ class TodoListViewer:
         self.screen = None
         self.scroll_offset = 0
         self.selected_line = 0
-        self.quit = False
+        self.close = False
         self.items = []
 
-    def init_screen(self):
+    def run(self, *_):
+        """ Shows the viewer and enters a rendering loop.
+        """
+        try:
+            self._init()
+            self._read_todo_file()
+            while not self.close:
+                self._render()
+                self._handle_input()
+        except KeyboardInterrupt:
+            pass
+
+    def _init(self):
         self.screen = curses.initscr()
         self.screen.keypad(1)
         self.screen.border(0)
         curses.noecho()
-        curses.cbreak()
+        # curses.cbreak()
         curses.curs_set(0)
-        self.init_colors()
-
-    def init_colors(self):
         curses.start_color()
         for color_index, color in enumerate(COLORS):
             red, green, blue = hex_to_rgb(color)
             curses.init_color(color_index + 1, red, green, blue)
             curses.init_pair(color_index + 1, color_index + 1, 0)
 
-    def read_todo_file(self):
+    def _read_todo_file(self):
         self.items.clear()
         with open(os.path.join(self.root, 'todo.txt'), 'r') as todofile:
             self.items = sorted([(index + 1, line)
-                                 for index, line in enumerate(todofile.readlines())], key=lambda x: get_priority_as_number(x))
+                                 for index, line in enumerate(todofile.readlines())], key=get_priority_as_number)
 
-    def run(self, *args, **kwargs):
-        try:
-            self.init_screen()
-            self.read_todo_file()
-            while not self.quit:
-                self.display_screen()
-                self.handle_input()
-        except KeyboardInterrupt:
-            pass
-
-    def handle_input(self):
+    def _handle_input(self):
         c = self.screen.getch()
         if c == curses.KEY_UP:
-            self.scroll(-1)
+            self._scroll(-1)
         elif c == curses.KEY_DOWN:
-            self.scroll(1)
-        elif c == 27:  # ESC_KEY
-            self.quit = True
+            self._scroll(1)
+        elif c == ord('q') or c == 27:  # ESC_KEY
+            self.close = True
+        elif c == ord('d') or c == curses.KEY_ENTER:
+            Dialog().run()
 
-    def print_item(self, index, item, selected=False):
-        color_index = get_priority_as_number(item, maximum=len(COLORS) - 1) + 1
-        attr = curses.color_pair(color_index) | (
-            selected and curses.A_BOLD or 0)
-        linenum, line = item
-        line = re.sub(RE_PRIORITY + ' ', '', line)
-        line = re.sub(RE_CONTEXT_OR_PROJECT, r'\1', line)
-        self.screen.addnstr(
-            index, 0, '{:02d} {:}'.format(linenum, line), 80, attr)
-
-    def display_screen(self):
-        self.screen.erase()
-        top = self.scroll_offset
-        bottom = self.scroll_offset + self.num_available_lines + 1
-        for index, item in enumerate(self.items[top:bottom]):
-            self.print_item(index, item)
-        self.print_item(self.selected_line - self.scroll_offset,
-                        self.items[self.selected_line], True)
-        self.screen.refresh()
-
-    def scroll(self, delta):
+    def _scroll(self, delta):
         self.selected_line = max(
             0, min(len(self.items) - 1, self.selected_line + delta))
         if self.selected_line < self.scroll_offset:
             self.scroll_offset = self.selected_line
         elif self.selected_line > self.num_available_lines + self.scroll_offset:
             self.scroll_offset = self.selected_line - self.num_available_lines
+
+    def _print_item(self, index, item, selected=False):
+        color_index = get_priority_as_number(item, maximum=len(COLORS) - 1) + 1
+        attr = curses.color_pair(color_index) | (
+            selected and curses.A_REVERSE or 0)
+        linenum, line = item
+        line = re.sub(RE_PRIORITY + ' ', '', line)
+        line = re.sub(RE_CONTEXT_OR_PROJECT, r'\1', line)
+        self.screen.addnstr(
+            index, 0, '{:02d} {:}'.format(linenum, line), 80, attr)
+
+    def _render(self):
+        self.screen.erase()
+        top = self.scroll_offset
+        bottom = self.scroll_offset + self.num_available_lines + 1
+        for index, item in enumerate(self.items[top:bottom]):
+            self._print_item(index, item)
+        self._print_item(self.selected_line - self.scroll_offset,
+                         self.items[self.selected_line], True)
+        self.screen.refresh()
 
 
 def main():
